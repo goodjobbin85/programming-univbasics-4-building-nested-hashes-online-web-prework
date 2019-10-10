f63aebcc14c48fe7051ec2fea32a3ede6cfe25ce
thomas dubiel <td1729@hotmail.com>
2019-10-10 01:16:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,7 +61,7 @@ def monopoly_with_fourth_tier
 	        "mortgage_value" => "$400"
 	      },
 	      :shortline_railroad => {
-	        "mortgage_value" => "$1600"
+	        "mortgage_value" => "$800"
 	      }
 	    },
 	    :rent_in_dollars => {
