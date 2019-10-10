8d71acfe3c673e17044d2e18c61e0409880ed759
thomas dubiel <td1729@hotmail.com>
2019-10-10 00:44:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,12 @@ def monopoly_with_third_tier
   {
 	  :railroads => {
 	    :pieces => 4,
-	    :rent_in_dollars => {}
+	    :rent_in_dollars => {
+	      :one => {},
+	      :two => {},
+	      :three => {},
+	      :four => {}
+	    }
 	  }
   	}
 end
